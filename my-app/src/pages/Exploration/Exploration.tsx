52a4c941ef2b3044d5975b31c9e1a35de83e1618
Louis Sanna <louis.sanna@ekimetrics.com>
2022-03-08 22:32:41 +0100

    Add response to question

--- my-app/src/pages/Exploration/Exploration.tsx
+++ my-app/src/pages/Exploration/Exploration.tsx
@@ -4,6 +4,7 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 
 import { Layout } from '../../shared/Layout';
+import { useState } from 'react';
 
 
 export { Exploration };
@@ -11,17 +12,18 @@ export { Exploration };
 interface Question {
     id: number;
     text: string;
-    answer?: boolean;
+    answer?: boolean | null;
 }
 
-const questions = [
+const questionBank = [
     { id: 0, text: "Do you like driving?" },
     { id: 1, text: "Do you live in a city?" },
 ] as Question[];
 
 function Exploration() {
 
-    const question = getQuestion();
+    const [questions, setQuestions] = useState<Question[]>(questionBank);
+    const question = getQuestion(questions);
   
     return (<Layout>
         <Grid>
@@ -29,17 +31,54 @@ function Exploration() {
             <Paper sx={{ padding: '20px', width: "700px", margin: "auto" }}>{question.text}</Paper>
             <div style={{ display: "flex", marginTop: "20px" }}>
                 <ButtonGroup disableElevation variant="contained" sx={{ margin: "auto" }}>
-                    <Button>Yes</Button>
-                    <Button>Don't know</Button>
-                    <Button>No</Button>
+                    <Button onClick={() => setQuestions(questions.map((_question) => {
+                        if (_question.id !== question.id) {
+                            return _question;
+                        }
+                        return {..._question, answer: true };
+                    }))}>
+                        Yes
+                    </Button>
+                    <Button onClick={() => setQuestions(questions.map((_question) => {
+                        if (_question.id !== question.id) {
+                            return _question;
+                        }
+                        return {..._question, answer: null };
+                    }))}>
+                        Don't know
+                    </Button>
+                    <Button onClick={() => setQuestions(questions.map((_question) => {
+                        if (_question.id !== question.id) {
+                            return _question;
+                        }
+                        return {..._question, answer: false };
+                    }))}>
+                        No
+                    </Button>
                 </ButtonGroup>
             </div>
         </Grid>
       </Layout>);
 }
 
-function getQuestion(): Question {
-    const unansweredQuestions = questions.filter(question => typeof question.answer === "undefined");
-    const randomIndex = Math.floor(Math.random() * questions.length);
+function getQuestion(questions: Question[]) {
+    const unansweredQuestions = getUnansweredQuestions(questions);
+    if (unansweredQuestions.length === 0) {
+        return {
+            id: -1,
+            text: 'no question left',
+            answer: null,
+        }
+    }
+    const randomIndex = getRandomIndex(questions);
     return unansweredQuestions[randomIndex];
+}
+
+function getRandomIndex(questions: Question[]): number {
+    const unansweredQuestions = getUnansweredQuestions(questions);
+    return Math.floor(Math.random() * unansweredQuestions.length);
+}
+
+function getUnansweredQuestions(questions: Question[]): Question[] {
+    return questions.filter(question => typeof question.answer === "undefined");
 }
