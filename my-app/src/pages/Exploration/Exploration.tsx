bc36b220e3da8c7c37fb15d909fcd27205ac1c8d
Louis Sanna <louis.sanna@ekimetrics.com>
2022-03-08 22:08:35 +0100

    Add questions

--- my-app/src/pages/Exploration/Exploration.tsx
+++ my-app/src/pages/Exploration/Exploration.tsx
@@ -1,3 +1,8 @@
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
+
 import { Layout } from '../../shared/Layout';
 
 
@@ -6,6 +11,16 @@ export { Exploration };
 function Exploration() {
   
     return (<Layout>
-        <h1>Exploration</h1>
+        <Grid>
+            <h1>Exploration</h1>
+            <Paper sx={{ padding: '20px', width: "700px", margin: "auto" }}>Do you like driving?</Paper>
+            <div style={{ display: "flex", marginTop: "20px" }}>
+                <ButtonGroup disableElevation variant="contained" sx={{ margin: "auto" }}>
+                    <Button>Yes</Button>
+                    <Button>Don't know</Button>
+                    <Button>No</Button>
+                </ButtonGroup>
+            </div>
+        </Grid>
       </Layout>);
   }
