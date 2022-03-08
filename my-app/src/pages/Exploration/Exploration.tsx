a14e70474cffd961a19e362698b28c5e81e8ae19
Louis Sanna <louis.sanna@ekimetrics.com>
2022-03-08 22:15:17 +0100

    Randomize questions

--- my-app/src/pages/Exploration/Exploration.tsx
+++ my-app/src/pages/Exploration/Exploration.tsx
@@ -8,12 +8,25 @@ import { Layout } from '../../shared/Layout';
 
 export { Exploration };
 
+interface Question {
+    id: number;
+    text: string;
+    answer?: boolean;
+}
+
+const questions = [
+    { id: 0, text: "Do you like driving?" },
+    { id: 1, text: "Do you live in a city?" },
+] as Question[];
+
 function Exploration() {
+
+    const question = getQuestion();
   
     return (<Layout>
         <Grid>
             <h1>Exploration</h1>
-            <Paper sx={{ padding: '20px', width: "700px", margin: "auto" }}>Do you like driving?</Paper>
+            <Paper sx={{ padding: '20px', width: "700px", margin: "auto" }}>{question.text}</Paper>
             <div style={{ display: "flex", marginTop: "20px" }}>
                 <ButtonGroup disableElevation variant="contained" sx={{ margin: "auto" }}>
                     <Button>Yes</Button>
@@ -23,4 +36,10 @@ function Exploration() {
             </div>
         </Grid>
       </Layout>);
-  }
+}
+
+function getQuestion(): Question {
+    const unansweredQuestions = questions.filter(question => typeof question.answer === "undefined");
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    return unansweredQuestions[randomIndex];
+}
